acfe9ab652264e54161d1f40c221625cb76e6376
Daniel Pascual López <uo269728@uniovi.es>
2022-02-11 10:24:38 +0100

    SDI-2122-317-2.1-Create Spring project.

---
+++ src/main/java/com/uniovi/sdi2122317spring/Sdi2122317SpringApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2122317spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2122317SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2122317SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniovi/sdi2122317spring/Sdi2122317SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2122317spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2122317SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
